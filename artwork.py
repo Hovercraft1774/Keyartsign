e5dc0a7aac047aff407aa090253af0611cd5d011
Tyler Johnson <hovercraft1774@gmail.com>
2022-09-28 12:17:22 -0600

    Initial commit

---
+++ artwork.py
@@ -0,0 +1,63 @@
+#Tyler Johnson
+# 9/27/22
+# yay keyart
+
+#Goals
+
+# make input for artist name
+# make/find a key image
+# attempt to make print statements to correctly put in image
+# make space at bottom of key image for author name
+# put author name into the bottom of key image through signature
+# Make sure that the input only goes for 10 characters.
+# Make sure that it keeps the same format with picture
+# frame your signed artwork
+
+
+#imports
+import math #as is customary
+import random
+
+
+#        Va     Rye      Ah      Blehs
+
+
+
+
+
+
+
+
+#      coding initiating?   MAIN
+
+
+def main():
+
+    author = input("What is the Authors name?"+"\t")
+    signature = str(str.format("{0:10.10}",author)) #converts author name into signature, and shortens it to 10 characters
+
+    #cue massive prints for the art :>
+    print()#give some space for the glorious thing
+    print()
+    print("  _________________________________________________")
+    print(" /"+"  \t\t\t\t\t\t\t\t\t\t\t   "+"\ ")
+    print("!\t\t░░░░░░░░░░░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░░█░░░░░░░░▀▄░░░░░░▄░░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░█░░▀░░▀░░░░░▀▄▄░░█░█░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░█░▄░█▀░▄░░░░░░░▀▀░░█░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░█░░▀▀▀▀░░░░░░░░░░░░█░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░░█░░▄▄░░▄▄▄▄░░▄▄░░█░░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░░█░▄▀█░▄▀░░█░▄▀█░▄▀░░░░░░░░░░\t\t!")
+    print("!\t\t░░░░░░░░░░░░▀░░░▀░░░░░▀░░░▀░░░░░░░░░░░░\t\t!")
+    print(" \\" + "\t\t\t\t\t\t\t\t\t\t\t\t    " + "/")
+    print("  --------------------------------------------------")
+
+    # print(signature)
+
+
+
+
+
+main()
